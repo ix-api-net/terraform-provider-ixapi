529e9e3758c2d14b589590eaa076ddb8c2df1c73
Annika Hannig <annika@hannig.cc>
2022-08-22 18:46:31 +0200

    initial metro area datasource, added doc generator

---
+++ internal/datasources/metro_area.go
@@ -0,0 +1,8 @@
+package datasources
+
+import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+// NewMetroAreaDataSource creates a new metro area datasource.
+func NewMetroAreaDataSource() *schema.Resource {
+
+}
