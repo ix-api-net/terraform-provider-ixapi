56cfa8a973488c762fa8007ec13518bd6f90bea7
Annika Hannig <annika@hannig.cc>
2022-09-23 13:57:23 +0200

    generalize type

--- internal/filter/string.go
+++ internal/filter/string.go
@@ -2,9 +2,32 @@ package filter
 
 import "strings"
 
-// String compares strings, case insensitive.
-// Returns true if the strings do not match, so the
+// Missing compares values, strings case insensitive.
+// Returns true if the values do not match, so the
 // result can be skipped.
-func String(a string, b any, apply bool) bool {
-	return apply && (strings.ToLower(a) != strings.ToLower(b.(string)))
+func Missing(a any, b any, apply bool) bool {
+	if !apply {
+		return false
+	}
+
+	var v1, v2 string
+	switch v := a.(type) {
+	case string:
+		v1 = v
+	case *string:
+		if v == nil {
+			return true
+		}
+		v1 = *v
+	}
+	switch v := b.(type) {
+	case string:
+		v2 = v
+	case *string:
+		if v == nil {
+			return true
+		}
+		v2 = *v
+	}
+	return strings.ToLower(v1) != strings.ToLower(v2)
 }
--- internal/filter/string_test.go
+++ internal/filter/string_test.go
@@ -2,16 +2,27 @@ package filter
 
 import "testing"
 
-func TestStringFilter(t *testing.T) {
+func TestMissingFilter(t *testing.T) {
 	a := "foo"
 
-	if String(a, "fOO", true) {
+	if Missing(a, "fOO", true) {
 		t.Error("should return false, because string is present")
 	}
-	if String(a, "", false) {
+	if Missing(a, "", false) {
 		t.Error("should return false, because filter is not present")
 	}
-	if !String(a, "bar", true) {
+	if !Missing(a, "bar", true) {
 		t.Error("should return true, because filter does not match")
+	}
+
+	// Optional strings
+	var opt *string = &a
+	if Missing(opt, "foo", true) {
+		t.Error("should return true, because opt is foo")
+	}
+
+	opt = nil
+	if !Missing(opt, "foo", true) {
+		t.Error("should not return true, because opt is nil")
 	}
 }
