8804601375f7ed8a6b86606a97f1a730c4518c93
Annika Hannig <annika@hannig.cc>
2022-10-28 14:44:15 +0200

    test schema versions

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -1,10 +1,22 @@
 package provider
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/ix-api-net/terraform-provider-ix-api/internal/ixapi"
+	"github.com/ix-api-net/terraform-provider-ix-api/internal/schemas"
+)
 
 func TestProvider(t *testing.T) {
 	p := New("test")
 	if p == nil {
 		t.Fatal("could not create provider instance")
+	}
+}
+
+func TestSchemaVersions(t *testing.T) {
+	if ixapi.SchemaVersion != schemas.SchemaVersion {
+		// In this case: Regenerate client / schema files
+		t.Fatal("client schema version must match terraform schema version")
 	}
 }
